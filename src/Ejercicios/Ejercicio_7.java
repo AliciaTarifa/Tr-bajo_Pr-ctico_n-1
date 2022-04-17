f7cca7789a010d3e0608b40f0cfea063c5b49981
Alicia <alicia.tarifa.8@gmail.com>
2022-04-17 20:44:08 -0300

    Ejercicio 7

---
+++ src/Ejercicios/Ejercicio_7.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_7 {
+
+	/*
+	 * Declarar variables de tipo cadena de caracteres y mostrarlas por pantalla
+	 */
+	
+	public static void main(String[] args) {
+		
+		
+		String saludo = "Buen día, ";
+		String nombre = new String("Josefina Isabel");
+		
+		System.out.println(saludo + nombre);
+		
+		String cliente = "Felipe Farfán";
+		String numeroCliente = "2445";
+		String DNI = new String ("31784621");
+		
+		System.out.println("El cliente " + cliente + ", número " + numeroCliente + ", tiene el DNI número " + DNI);
+
+	}
+
+}
