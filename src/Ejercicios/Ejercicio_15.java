c4c82e1a6ad70d67ee9f16d0e6bb024b31d0986a
Alicia <alicia.tarifa.8@gmail.com>
2022-04-18 20:19:13 -0300

    Ejercicio 15

---
+++ src/Ejercicios/Ejercicio_15.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_15 {
+
+	/*
+	 * Será posible almacenar un tipo de dato char en un tipo entero? De ser posible explique
+	 * con comentarios el porque.
+	 */
+	public static void main(String[] args) {
+		
+	
+		char micaracter = 36;
+		int entero = micaracter;
+		
+		System.out.println (micaracter);
+		System.out.println (entero);
+
+	}
+	
+	/*
+	 * Si se puede almacenar un tipo de dato char en un entero, debido a que char utiliza un
+	 * número entero , un int, para dibujar el caracter solicitado.
+	 */
+
+}
