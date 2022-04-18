03527a12497e902404bf243ac1b437a732b0cba2
Alicia <alicia.tarifa.8@gmail.com>
2022-04-18 10:31:10 -0300

    Ejercicio 10

---
+++ src/Ejercicios/Ejercicio_10.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author alicia
+ *
+ */
+public class Ejercicio_10 {
+
+	/*
+	 * Muestre en un ejemplo cómo se usan los operadores incremento y decremento.
+	 */
+	public static void main(String[] args) {
+		
+		int numero = 6;
+		int numero2 = 9;
+		
+		int incremento = ++numero;
+		int decremento = --numero2;
+		
+		System.out.println("El operador de incremento da por resultado " + incremento);
+		System.out.println("El operador de decremento da por resultado " + decremento);
+
+	}
+
+}
