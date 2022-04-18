45d28384e74918edfeb6e932ef1f9009e3afe237
Alicia <alicia.tarifa.8@gmail.com>
2022-04-18 17:38:39 -0300

    Ejercicio 12

--- src/Ejercicios/Ejercicio_12.java
+++ src/Ejercicios/Ejercicio_12.java
@@ -17,14 +17,14 @@ public class Ejercicio_12 {
 		
 		int numero1 = 88;   int luis = 35;
 		int numero2 = 112;  int tiziano = 13;
-		int numero3 = 88;   int felipe = 10;
+		int numero3 = 68;   int felipe = 10;
 		int numero4 = 179;  int matias = 24;
 		
 		if (numero1 < 100 && numero3 < 100) {
-			System.out.println("Ambos números son menores de 100");
+			System.out.println(numero1 + " y " + numero3 + " son menores de 100");
 		}
 		if (numero2 > 100 && numero4 >100) {
-			System.out.println("Ambos números son mayores de 100");
+			System.out.println(numero2 + " y " + numero4 + " son mayores de 100");
 		}
 		if (luis > 18 || matias >18) {
 			System.out.println("Luis y Matias son adultos");
