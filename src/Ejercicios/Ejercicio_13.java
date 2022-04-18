cf0ce4c04bb1b92df87d578067034bfc37728db7
Alicia <alicia.tarifa.8@gmail.com>
2022-04-18 18:16:30 -0300

    Ejercicio 13

---
+++ src/Ejercicios/Ejercicio_13.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package Ejercicios;
+
+/**
+ * @author luise
+ *
+ */
+public class Ejercicio_13 {
+
+	/*
+	 * Realizar un programa que calcule el resultado de una expresion algebraica, use distintos
+	 * operadores. Realizar al menos 3 expresiones algebraicas.
+	 */
+	
+	public static void main(String[] args) {
+		
+		int operacion1 = 88 + (65*4) - 15;
+		double operacion2 = 3.1 / 8.6 + 6.8 * 7.68;
+		double operacion3 = 15 + 26.8 + (8 * 4.2)- 8 / (2 + 1.5);
+		
+		System.out.println("El resultado de la primer operación es " + operacion1);
+		System.out.println("El resultado de la segunda operación es " + operacion2);
+		System.out.println("El resultado de la tercera operación es " + operacion3);
+	}
+
+}
